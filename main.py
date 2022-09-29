817b8be938f1d73cfb75cdf6e347da6039f53917
Anastasiya Semizorova <semizorova2205@mail.ru>
2022-09-29 23:00:16 +0700

    Удален файл с генерацией и переписан main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 
 from image_generate import generate
+from imagehangul import ImageHangul
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DEFAULT_LABEL_FILE = 'labels.txt'
 DEFAULT_FONTS_DIR = os.path.join(SCRIPT_PATH, 'fonts')
-DEFAULT_OUTPUT_DIR = os.path.join(SCRIPT_PATH, 'image-data')
 
 if __name__ == '__main__':
-    generate(DEFAULT_LABEL_FILE, DEFAULT_FONTS_DIR, DEFAULT_OUTPUT_DIR)
+    imagehangul = ImageHangul()
+    imagehangul.generate(DEFAULT_LABEL_FILE, DEFAULT_FONTS_DIR)
+    print('Сгенерировано {} изображений.'.format(imagehangul.count))
